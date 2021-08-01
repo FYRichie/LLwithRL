0895474383aabaac598e356401112e40cc79d94a
傅譽的MacBook <b08901039@ntu.edu.tw>
2021-08-02 00:01:28 +0800

    add commit

--- model.py
+++ model.py
@@ -10,4 +10,5 @@ class Model(nn.Module):
         self.__critic_network = config.critic_network
         
     def forward(self, x):
+        # unsure about how to do with critic network
         pass
